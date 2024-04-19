6ec86ccefa6f8cf65114b111eb5e29dfaf2d2a28
Eric Z. Ayers <ericzundel@gmail.com>
2024-04-19 14:10:16 -0400

    Added a script to convert a tensorflow model to tensorflow lite

---
+++ convert_model_to_tf_lite.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+import os
+
+saved_model_dir = "./2024model/student_recognition.tf"
+
+# Convert the model
+converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir) # path to the SavedModel directory
+tflite_model = converter.convert()
+
+# Save the model.
+with open('model.tflite', 'wb') as f:
+  f.write(tflite_model)
+
