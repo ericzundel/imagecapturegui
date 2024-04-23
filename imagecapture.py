b0ae96a5cc8c7f120defd35eaa329abf95a4aa22
Eric Z. Ayers <ericzundel@gmail.com>
2024-04-23 08:03:46 -0400

    Switched rasperry pi to use mplayer

--- imagecapture.py
+++ imagecapture.py
@@ -355,7 +355,7 @@ def text_to_speech(text):
     if (platform_name == "Windows"):
         os.system(f"start {filename}")
     elif (platform_name == "Linux"):
-        os.system(f"aplay {filename}")
+        os.system(f"mplayer {filename}")
     else:
         print("Update script for how to play sound on %s" % platform_name)
 
