6ac8beeb49342d2219a23f90b6bb159485eae35f
Eric Z. Ayers <ericzundel@gmail.com>
2024-04-23 07:57:51 -0400

    Updated converter after converting to 32 bit

--- convert_model_to_tf_lite.py
+++ convert_model_to_tf_lite.py
@@ -1,13 +1,15 @@
 import tensorflow as tf
 import os
 
-saved_model_dir = "./2024model/student_recognition.tf"
+saved_model_dir = "./2024model/"
+
+model_name = "student_recognition_2024_32bit"
 
 # Convert the model
-converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir) # path to the SavedModel directory
+converter = tf.lite.TFLiteConverter.from_saved_model("%s/%s.tf" % (saved_model_dir, model_name)) # path to the SavedModel directory
 tflite_model = converter.convert()
 
 # Save the model.
-with open('model.tflite', 'wb') as f:
+with open('%s/%s.tflite' % (saved_model_dir, model_name), 'wb') as f:
   f.write(tflite_model)
 
